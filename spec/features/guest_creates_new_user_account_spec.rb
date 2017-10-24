a1f0a0b51f1a1e02c134248969749fd4cea9c65b
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-23 20:31:47 -0600

    add feature test

---
+++ spec/features/guest_creates_new_user_account_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+
+describe "guest becomes user" do
+  scenario "guest signs up to be new user" do
+
+    visit '/'
+
+    click_on "Create New Account"
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in "user[username]", with: "user"
+    fill_in "user[password]", with: "test"
+    fill_in "user[first_name]", with: "first"
+    fill_in "user[last_name]", with: "last"
+    fill_in "user[slack_handle]", with: "slack"
+    fill_in "user[cohort]", with: 1234
+
+    click_on "Create User"
+
+    expect(page).to have_content("Welcome, user!")
+  end
+end
