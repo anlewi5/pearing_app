a2e3ed331b1eba3b2513fc535f3cdc29f59584fb
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-24 12:13:09 -0600

    create request migration

--- db/migrate/20171024180504_create_requests.rb
+++ db/migrate/20171024180504_create_requests.rb
@@ -1,11 +1,12 @@
 class CreateRequests < ActiveRecord::Migration[5.1]
   def change
     create_table :requests do |t|
-      t.string :day
-      t.string :start_time
-      t.string :duration
+      t.string :title
+      t.date :day
+      t.time :start_time
+      t.integer :duration
       t.string :location
-      t.string :description
+      t.text :description
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024023353) do
+ActiveRecord::Schema.define(version: 20171024180504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "requests", force: :cascade do |t|
+    t.string "title"
+    t.date "day"
+    t.time "start_time"
+    t.integer "duration"
+    t.string "location"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -24,4 +37,5 @@ ActiveRecord::Schema.define(version: 20171024023353) do
     t.integer "cohort"
   end
 
+  add_foreign_key "requests", "users"
 end
