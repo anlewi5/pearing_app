58ddc35a6be0fbe5d75e3da61db7234784094600
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-25 19:41:16 -0600

    add questionable seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Request.destroy_all
+
+USERS_FIRST= ["Anna", "Matt", "Hel", "Pass"]
+USERS_LAST = ["L", "Lewis", "Kitten", "Pupper"]
+REQUESTS = ["Help", "Why", "Nooo", "Please Send Help"]
+LOCATIONS = ["Vault", "Classroom", "Kitchen", "T"]
+
+USERS_FIRST.each do |name|
+  user = User.create!(username: "user",
+                      password: "pass",
+                      first_name: name,
+                      last_name: USERS_LAST.sample,
+                      slack_handle: "slack",
+                      cohort: rand(100)
+                     )
+  puts "Created #{user.first_name}"
+  4.times do |num|
+    user.requests.create!(title: REQUESTS.sample, day: Date.new(2017), start_time: Time.now, duration: rand(100), location: LOCATIONS.sample, description: "need help meow")
+    puts "Created #{user.requests[num].title}"
+  end
+end
