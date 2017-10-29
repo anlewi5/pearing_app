562ba8edf0d7b07bbce0327b24e9664673437e06
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-29 17:02:17 -0600

    add require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :require_login
+
   helper_method :current_user
 
   def current_user
@@ -9,5 +11,13 @@ class ApplicationController < ActionController::Base
 
   def current_admin?
     current_user && current_user.admin?
+  end
+
+  def current_admin?
+    current_user.user?
+  end
+
+  def require_login
+    render file: "/public/404" unless current_user?
   end
 end
